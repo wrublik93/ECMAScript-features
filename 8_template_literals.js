68aac108d9d915ef61506881499850c6ca1baf78
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-11-11 03:10:44 +0300

    feat: template literal

---
+++ 8_template_literals.js
@@ -0,0 +1,9 @@
+const user = 'Ivan';
+const age = 27;
+const text = `My name is ${user}. I am ${age} years old.`
+console.log(text);
+
+// в { } можно записывать выражения, они будут посчитаны
+
+// многострочный код
+// результат - обычна строка
